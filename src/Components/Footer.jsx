7cd689f5d48650548b217c365ffb21a8101ebb73
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-15 17:47:17 +0600

    footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,38 +1,35 @@
-import React from 'react';
+import logo from '../images/footer.png'
 
 const Footer = () => {
-    return (
-        <section>
-            <footer className="bg-gray-100 border-t py-4 px-6 text-sm text-gray-700 flex justify-between items-center">
-      {/* Left: Logo and Brand Name */}
-      <div className="flex items-center space-x-2">
-        <img src={travelBidLogo} alt="TravelBid Logo" className="h-6" />
-        <span className="font-semibold">
-          <span className="text-travel">Travel</span>
-          <span className="text-bid">Bid</span>
-        </span>
-      </div>
+  return (
+    <section className='bg-gray-100 mt-16'>
+      <footer className=" w-11/12 mx-auto border-t py-4 px-6 text-sm text-gray-700 flex justify-between items-center">
+        {/* Left: Logo and Brand Name */}
+        <div className="flex items-center space-x-2">
+          <img src={logo} alt="logo" className="w-36" />
+          
+        </div>
 
-      {/* Right: Links and Info */}
-      <div className="text-right text-gray-600">
-        <div>
-          <a href="#" className="text-blue-600 hover:underline">
-            Privacy
-          </a>{" "}
-          |{" "}
-          <a href="#" className="text-blue-600 hover:underline">
-            Terms of service
-          </a>
+        {/* Right: Links and Info */}
+        <div className="text-right text-gray-600">
+          <div>
+            <a href="#" className="text-blue-600 hover:underline">
+              Privacy
+            </a>{" "}
+            |{" "}
+            <a href="#" className="text-blue-600 hover:underline">
+              Terms of service
+            </a>
+          </div>
+          <div className="mt-1">
+            © 2003–2025 TravelBid Inc. All Rights Reserved.
+            <br />
+            TravelBid is a trademark of TravelBid Inc.
+          </div>
         </div>
-        <div className="mt-1">
-          © 2003–2025 TravelBid Inc. All Rights Reserved.
-          <br />
-          TravelBid is a trademark of TravelBid Inc.
-        </div>
-      </div>
-    </footer>
-        </section>
-    );
+      </footer>
+    </section>
+  );
 };
 
 export default Footer;
