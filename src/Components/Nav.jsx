3bef98d7ec0f92ff01790683c015d0cd00e7b057
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-18 00:21:56 +0600

    done for today

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -60,7 +60,7 @@ const Nav = () => {
                 <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                   {/* Sidebar content here */}
                   <li>
-                    <a>Sidebar Item 1</a>
+                    <a>All Visas</a>
                   </li>
                   <li>
                     <a>Sidebar Item 2</a>
