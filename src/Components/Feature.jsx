c27b5355741cffbe26fde1b8e6d293ff5e8da9c5
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-17 22:44:33 +0600

    two images added

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import turkey from '../images/turkey.jpg'
+import dubai from '../images/dubai.jpg'
 
 const Feature = () => {
     return (
         <section className='mt-10 py-3 lg:mt-20'>
             <div className='w-11/12 mx-auto'>
                 <h1 className='text-3xl font-bold lg:text-4xl text-center '>Our latest <span className='border-b-8 rounded-lg border-bidcl'>visas</span></h1>
-                <div className='grid grid-cols-3'>
-
+                <div className='grid grid-cols-3 gap-2 mt-10'>
+                   <div className='col-span-2 rounded-md'>
+                     <img className='h-full rounded-2xl' src={turkey} alt="" />
+                   </div>
+                    <img className='rounded-2xl ' src={dubai} alt="" />
                 </div>
             </div>
         </section>
