f18195f85d59adbde0dc70b4ffeaded22efe54ca
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-17 22:32:53 +0600

    feature section grid start

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Feature = () => {
     return (
-        <section>
-            
+        <section className='mt-10 py-3 lg:mt-20'>
+            <div className='w-11/12 mx-auto'>
+                <h1 className='text-3xl font-bold lg:text-4xl text-center '>Our latest <span className='border-b-8 rounded-lg border-bidcl'>visas</span></h1>
+                <div className='grid grid-cols-3'>
+
+                </div>
+            </div>
         </section>
     );
 };
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Visa from './Visa';
+import Feature from './Feature';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Visa></Visa>
+            <Feature></Feature>
         </div>
     );
 };
