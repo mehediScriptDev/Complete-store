0846e0d9673b0da211e58be73af11c889974254c
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-19 11:04:17 +0600

    form added

--- src/Components/Addvisa.jsx
+++ src/Components/Addvisa.jsx
@@ -4,24 +4,56 @@ const Addvisa = () => {
   return (
     <div>
       <div className="card-body bg-base-200 w-8/12 mx-auto mt-10">
+      <h1 className="text-center text-3xl font-bold text-travelcl py-3">Add <span className="text-bidcl">visa</span></h1>
         <form className="fieldset">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
             <div>
-                <label className="label">Visa title/Country name</label>
-            <input type="email" className="input w-full" placeholder="Email" />
+              <label className="label font-semibold text-sm">Visa title/Country name:</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Enter Country name/Visa title"
+                name="name"
+              />
             </div>
-           <div>
-             <label className="label">Country imageURL</label>
-            <input
-              type="text"
-              className="input w-full"
-              placeholder="Enter imageURL of that country"
-            />
-           </div>
+            <div>
+              <label className="label font-semibold text-sm">Country imageURL:</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Enter imageURL of that country"
+              />
+            </div>
+          </div>
+          
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+            <div>
+              <label className="label font-semibold text-sm">Min age:</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Enter minimum age to apply"
+                name="age"
+              />
+            </div>
+            <div>
+              <label className="label text-sm font-semibold">How long it taken:</label>
+              <input
+                type="text"
+                className="input w-full"
+                placeholder="Enter time itll take"
+                name="time"
+              />
+            </div>
+          </div>
+          <div className="">
+            <label className="label font-semibold text-sm">Add discription:</label>
+           
+            <textarea className="input w-full" name="discription" placeholder="Enter a little discription" id=""></textarea>
           </div>
 
           <button className="btn hover:bg-bidcl text-white bg-travelcl duration-300 mt-4">
-            Login
+            Add request
           </button>
         </form>
       </div>
