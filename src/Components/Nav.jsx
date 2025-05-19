ede489ca08a7a8fd7f9425f4d384b6fb493e4bd6
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-19 23:06:41 +0600

    nav section edited

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -15,9 +15,7 @@ const Nav = () => {
       <li>
         <NavLink to={'/faq'}>F.A.Q</NavLink>
       </li>
-      <li>
-        <NavLink to={'/about'}>About</NavLink>
-      </li>
+      
       <li>
         <NavLink to={'/contact'}>Contact</NavLink>
       </li>
