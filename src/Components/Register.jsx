35d66f090407371c897c9fa753da7c48acbacb95
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-22 21:08:49 +0600

    btn for google login added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../Auth/AuthProvider';
+import React, { useContext } from "react";
+import { AuthContext } from "../../Auth/AuthProvider";
 
 const Register = () => {
-
-  const {creatUser,setUser} = useContext(AuthContext);
-  const submitHandler = e => {
+  const { creatUser, setUser } = useContext(AuthContext);
+  const submitHandler = (e) => {
     e.preventDefault();
     const form = e.target;
     const name = form.name.value;
@@ -12,24 +11,28 @@ const Register = () => {
     const password = form.pass.value;
     const photo = form.pass.value;
 
-    creatUser(email,password)
-    .then(result => {
-      setUser(result.user)
-      console.log(result)
-    })
-    .catch(err=>console.log(err))
-  }
+    creatUser(email, password)
+      .then((result) => {
+        setUser(result.user);
+        console.log(result);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="bg-white p-8 rounded-xl shadow-xl w-full max-w-md">
-        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">Create an <span className='text-bidcl'>Account</span></h2>
+        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+          Create an <span className="text-bidcl">Account</span>
+        </h2>
 
         <form onSubmit={submitHandler} className="space-y-5">
           <div>
-            <label className="block mb-1 text-sm font-medium text-gray-600">Full Name</label>
+            <label className="block mb-1 text-sm font-medium text-gray-600">
+              Full Name
+            </label>
             <input
               type="text"
-              name='name'
+              name="name"
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Write your name"
@@ -37,10 +40,12 @@ const Register = () => {
           </div>
 
           <div>
-            <label className="block mb-1 text-sm font-medium text-gray-600">Email</label>
+            <label className="block mb-1 text-sm font-medium text-gray-600">
+              Email
+            </label>
             <input
               type="email"
-              name='email'
+              name="email"
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Write your email"
@@ -48,9 +53,11 @@ const Register = () => {
           </div>
 
           <div>
-            <label className="block mb-1 text-sm font-medium text-gray-600">PhotoURL</label>
+            <label className="block mb-1 text-sm font-medium text-gray-600">
+              PhotoURL
+            </label>
             <input
-            name='photo'
+              name="photo"
               type="text"
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -58,10 +65,12 @@ const Register = () => {
             />
           </div>
           <div>
-            <label className="block mb-1 text-sm font-medium text-gray-600">Password</label>
+            <label className="block mb-1 text-sm font-medium text-gray-600">
+              Password
+            </label>
             <input
               type="password"
-              name='pass'
+              name="pass"
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="••••••••"
@@ -74,10 +83,43 @@ const Register = () => {
           >
             Register
           </button>
+          <button className="btn bg-white text-black border-[#e5e5e5]">
+            <svg
+              aria-label="Google logo"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <path d="m0 0H512V512H0" fill="#fff"></path>
+                <path
+                  fill="#34a853"
+                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                ></path>
+                <path
+                  fill="#4285f4"
+                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                ></path>
+                <path
+                  fill="#fbbc02"
+                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                ></path>
+                <path
+                  fill="#ea4335"
+                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                ></path>
+              </g>
+            </svg>
+            Login with Google
+          </button>
         </form>
 
         <p className="mt-6 text-center text-sm text-gray-500">
-          Already have an account? <a href="/login" className="text-blue-600 hover:underline">Login</a>
+          Already have an account?{" "}
+          <a href="/login" className="text-blue-600 hover:underline">
+            Login
+          </a>
         </p>
       </div>
     </div>
